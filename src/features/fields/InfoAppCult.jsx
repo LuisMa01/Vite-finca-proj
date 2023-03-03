48f1477109cc6acab87e0c52664d85f5fdbe0025
LuisMa <lmanudr01@gmail.com>
2023-03-02 22:45:02 -0400

    Jueves mecho

--- src/features/fields/InfoAppCult.jsx
+++ src/features/fields/InfoAppCult.jsx
@@ -5,9 +5,12 @@ import { useGetDatesQuery, useUpdateDateMutation } from "./redux/appApiSlice";
 import { useGetActsQuery } from "./redux/actApiSlice";
 import { useGetUsersQuery } from "./redux/usersApiSlice";
 import { useGetItemsQuery } from "./redux/itemApiSlice";
+import { useGetCostsQuery, useAddNewCostMutation } from "./redux/costApiSlice";
+import { Link } from "react-router-dom";
+import ReImage from "../../images/return.svg";
+import Cost from "../../components/Cost";
 
-
-const User = ({ userId }) =>{
+const User = ({ userId }) => {
   const { user } = useGetUsersQuery("usersList", {
     selectFromResult: ({ data }) => ({
       user: data?.entities[userId],
@@ -15,11 +18,11 @@ const User = ({ userId }) =>{
   });
 
   if (user) {
-    const usuario = user.user_nomnbre ? user.user_nombre: user.user_name
-    return(<>{usuario}</>)
+    const usuario = user.user_nomnbre ? user.user_nombre : user.user_name;
+    return <>{usuario}</>;
   }
-}
-const Act = ({ actId }) =>{
+};
+const Act = ({ actId }) => {
   const { act } = useGetActsQuery("actsList", {
     selectFromResult: ({ data }) => ({
       act: data?.entities[actId],
@@ -27,15 +30,28 @@ const Act = ({ actId }) =>{
   });
 
   if (act) {
-    return(<>{act.act_name}</>)
+    return <>{act.act_name}</>;
   }
-}
+};
+
 const InfoAppCult = () => {
   const [actKey, setActKey] = useState("");
   const [dateInit, setDateInit] = useState(null);
   const [dateEnd, setDateEnd] = useState(null);
   const [userRep, setUserRep] = useState("");
+  const [costItemKey, setItemCostKey] = useState("");
+  const [itemPrecio, setItemPrecioKey] = useState();
   const { id } = useParams();
+
+  const {
+    data: costs,
+    isError: costIsError,
+    error: costError,
+  } = useGetCostsQuery("costsList", {
+    pollingInterval: 60000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
 
   const { date } = useGetDatesQuery("datesList", {
     selectFromResult: ({ data }) => ({
@@ -43,25 +59,59 @@ const InfoAppCult = () => {
     }),
   });
   const { data: items } = useGetItemsQuery("itemsList");
-  
 
+  const onItemCostChanged = (e) => {
+    //e.preventDefault();
+    setItemCostKey(e.target.value);
+    if (items) {
+      const { entities } = items;
+      setItemPrecioKey(`${entities[e.target.value].item_price}`)
+    }
+  };
+
+  const [
+    addNewCost,
+    { isSuccess: addCostSuc, isError: addCostIserror, error: addCosterror },
+  ] = useAddNewCostMutation();
+
+  const onAddCostClicked = async (e) => {
+    e.preventDefault();
+    console.log(
+      `${actKey} ${userRep} ${dateInit} ${dateEnd} ${crop.crop_id} ${crop.crop_plant_key}`
+    );
+    await addNewCost({
+      actKey,
+      userRep,
+      dateInit,
+      dateEnd,
+      cropKey: crop.crop_id,
+      plantId: crop.crop_plant_key,
+    });
+  };
+  //  costItemKey, costQuantity, costDateKey
   useEffect(() => {
     if (date) {
       setActKey(date.date_act_key);
       setDateInit(date.date_init);
       setDateEnd(date.date_end);
       setUserRep(date.date_user_key);
+      setItemCostKey("");
     }
   }, [date]);
 
-  let itemOption
+  let itemOption;
+
   if (items) {
     const { ids, entities } = items;
 
     itemOption = ids.map((Id) => {
+
       if (entities[Id].item_status) {
         return (
-          <option key={Id} value={entities[Id].item_id}>
+          <option
+            key={Id}
+            value={entities[Id].item_id}
+          >
             {entities[Id].item_name}
           </option>
         );
@@ -69,31 +119,118 @@ const InfoAppCult = () => {
     });
   }
 
-  if(date){
-  const usuario = (<><User key={userRep} userId={userRep} /></>)
-  const actividad = (<><Act key={actKey} actId={actKey} /></>)
-  
-    
-  
+  let costList;
+  if (costs) {
+    const { ids, entities } = costs;
 
-  return (
-    <>
-      <div>ok {usuario} {actividad} {dateEnd} {dateInit}</div>
-      <div className="col-md-6 col-lg-3 mb-3">
-            <label htmlFor="campo_cultivo">Articulos</label>
-            <select
-              className="form-control"
-              
-            >
-              <option disabled value={""}>Elegir Acticulo</option>
-              {itemOption}
-            </select>
+    costList =
+      ids?.length &&
+      ids.map((Id) => {
+        if (entities[Id].cost_date_key == date.date_id) {
+          return <Cost key={Id} costId={Id} />;
+        }
+      });
+  }
+  if (costIsError) {
+    console.log(costError?.data?.message);
+    
+  }
+  if (date) {
+    const usuario = (
+      <>
+        <User key={userRep} userId={userRep} />
+      </>
+    );
+    const actividad = (
+      <>
+        <Act key={actKey} actId={actKey} />
+      </>
+    );
+
+    return (
+      <>
+        <div className="return-div">
+          <Link to={"/dash/cultivos"}>
+            <div className="return-button">
+              <img className="return-button-img" src={ReImage} alt="Atrás" />
+            </div>
+          </Link>
+        </div>
+        <div className="nuevo-cultivo-header">
+          MATERIALES, INSUMOS Y MANO DE OBRA {usuario} {actividad} {dateEnd}{" "}
+          {dateInit}
+        </div>
+        <form>
+          <div className="new-activity-miniform d-flex justify-content-center col-12 col-md-10 col-xl-9 form-row bg-light">
+            <div className="col-md-6 col-lg-3 mb-3">
+              <label htmlFor="campo_cultivo">Articulos</label>
+              <select
+                className="form-control"
+                value={costItemKey}
+                onChange={onItemCostChanged}
+              >
+                <option disabled value={""}>
+                  Elegir Acticulo
+                </option>
+                {itemOption}
+              </select>
+            </div>
+            <div className="col-md-6 col-lg-3 mb-3">
+              <label htmlFor="campo_cultivo">Precio</label>
+              <input
+                type="number"
+                className="form-control"
+                value={"123"}
+              />
+            </div>
+
+            <div className="cultivos_button-section">
+              <button
+                className="btn btn-success"
+                onClick={onAddCostClicked}
+                type="submit"
+              >
+                Agregar Artículo
+              </button>
+            </div>
           </div>
-    </>
-  );
-}else{
-  return null
-}
+        </form>
+
+        <div className="table-container col-12 col-md-9 col-xl-6">
+          <table className="table table-hover table-sm table-striped table-responsive-sm table-bordered">
+            <thead className="thead-loyola">
+              <th className="align-middle" scope="col">
+                Articulo
+              </th>
+              <th className="align-middle" scope="col">
+                Dosis
+              </th>
+              <th className="align-middle" scope="col">
+                Cantidad
+              </th>
+              <th className="align-middle" scope="col">
+                Unidad
+              </th>
+              <th className="align-middle" scope="col">
+                Costo Unitario (RD$)
+              </th>
+              <th className="align-middle" scope="col">
+                Costo Total (RD$)
+              </th>
+              <th className="align-middle" scope="col">
+                Eliminar
+              </th>
+            </thead>
+            <tbody>
+              <>{costList}</>
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default InfoAppCult;
