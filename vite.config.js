68d680cc1aab362c618978b6b3af874898f150f0
LuisMa <lmanudr01@gmail.com>
2023-04-27 09:07:38 -0400

    Jjueves no mine

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   
-  server: {
+  //server: {
     //host: "127.0.0.1",
       
-  },
+ // },
   plugins: [react()],
 });
 
